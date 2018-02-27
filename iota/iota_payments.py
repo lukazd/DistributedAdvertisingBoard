cec4b4f9ed030d719afb42b5d6ffbb8bdffffd51
Luka Zdilar <zdilarlu@gmail.com>
2018-02-27 01:32:08 -0600

    Modularize iota interfaces

--- iota/iota_payments.py
+++ iota/iota_payments.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from iota import Iota, ProposedTransaction, ProposedBundle, Address, Tag, TryteString
 from iota.adapter import HttpAdapter
@@ -8,34 +9,26 @@ IOTA_LOCAL_NODE = "http://localhost:14265"
 IOTA_REMOTE_NODE = "https://nodes.thetangle.org:443"
 SEED = os.environ['IOTA_SEED']
 
-# Remote node for all operations except POW
 api = Iota(
     RoutingWrapper(IOTA_REMOTE_NODE).add_route('attachToTangle', IOTA_LOCAL_NODE),
     seed = SEED)
 
-def get_balance(addresses):
-        return api.get_balances(addresses)['balances']
+def get_balance(address):
+    return api.get_balances(addresses)['balances'][0]
 
-def create_and_send_transactions(source_address, dest_address, amount, message):
-        transaction = ProposedTransaction(
-                address = Address(dest_address),
-                value = amount,
-                tag = None,
-                message = TryteString.from_string(message)
-        )
+def create_and_send_transactions(dest_address, amount, message):
+    transaction = ProposedTransaction(
+            address = Address(dest_address),
+            value = amount,
+            tag = None,
+            message = TryteString.from_string(message)
+    )
 
-        pt = api.prepare_transfer(transfers = [transaction], change_address = None)
-        api.send_trytes(pt['trytes'], depth = 100)
+    pt = api.prepare_transfer(transfers = [transaction], change_address = None)
+    api.send_trytes(pt['trytes'], depth = 100)
 
-def check_spent_address(address):
-    # TODO: Check for outbound transactions
-    api.find_transactions([addresses])
+def is_address_spent(address):
+    return api.were_addresses_spent_from(addresses = [address])['states'][0]
 
-source_address = "HXCGVJHWCFRXLXJUSTZRJACTGDNCHRSAKFKWEJIDJLZGYJG9ATUZWUOIXTZPAGWVIXHISLZNQJMPJNQID9JPOMOUKD"
-source_address, source_checksum = source_address[:-9], source_address[-9:]
-
-dest_address = "VEYONVNFFAQPKYMMOJZJ9JLQNBVGQMMLSDNTWZQYCYYNNJIBOKJHHGCIKKNEVEAXQO9MJXEQLFPQCIEAWDODLCPROX"
-dest_address, dest_checksum = dest_address[:-9], dest_address[-9:]
-
-print(get_balance([dest_address]))
-create_and_send_transactions(source_address, dest_address, 500, "Here is 500 iota.")
+def is_address_valid(address):
+    return re.search("[9A-Z]{81}", address) is not None
--- iota/qr_address_reader.py
+++ iota/qr_address_reader.py
@@ -6,20 +6,17 @@ from PIL import Image
 
 ADDRESS_IDENTIFIER = "address"
 
-def read_qr():
-    capture = cv2.VideoCapture(0)
-    while True:
-        ret, frame = capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def read_qr(identifier):
+    try:
+        capture = cv2.VideoCapture(0)
+        while True:
+            ret, frame = capture.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        image = Image.fromarray(gray)
-        decoded = decode(image, symbols=[ZBarSymbol.QRCODE])
+            image = Image.fromarray(gray)
+            decoded = decode(image, symbols=[ZBarSymbol.QRCODE])
 
-        if (len(decoded) > 0):
-            decoded_data = ast.literal_eval(decoded[0].data)
-            address = decoded_data.get(ADDRESS_IDENTIFIER)
-            if (address != None):
-                capture.release()
-                return(address)
-
-print(read_qr())
+            if (len(decoded) > 0):
+                return ast.literal_eval(decoded[0].data).get(identifier)
+    finally:
+        capture.release()
