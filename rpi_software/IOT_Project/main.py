f90ca5ebce59948658a0e6460a9b7fd48afec20e
BakirHajdarevic <bhajdarevic17@gmail.com>
2018-03-27 19:10:55 -0500

    GUI software changes

--- rpi_software/IOT_Project/main.py
+++ rpi_software/IOT_Project/main.py
@@ -1,38 +1,32 @@
+# Import main python library for numerical calculations
 import numpy as np
-import cv2
 from PIL import Image, ImageDraw
-import cognitive_face as CF
 import time
 import threading
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+# Import various kivy libraries
 import kivy
 kivy.require('1.10.0')
-#import Clock to create a schedule
-from kivy.clock import Clock
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.clock import Clock, mainthread
 from kivy.graphics import *
 from kivy.uix.widget import Widget
 from kivy.uix.dropdown import DropDown
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.properties import ObjectProperty, NumericProperty
+from kivy.properties import ObjectProperty
 from kivy.uix.image import AsyncImage
 from kivy.core.window import Window
 
-# Loading images asycnhorously:
-# https://kivy.org/docs/api-kivy.uix.image.html#kivy.uix.image.AsyncImage
-
-# Example of dynamic screen manager
-# https://stackoverflow.com/questions/34787525/kivy-changing-screen-from-python-code
-
-
+# Import camera object for python
+import cv2
 # Specify the camera to use, 0 = built-in
 cap = cv2.VideoCapture(0)
 
-KEY = 'bead87ccbe074693a5a793d101c8086d'
-#KEY = 'b7ae1171b8ad4b68b3839034902418ec'
+# Import
+import cognitive_face as CF
+
 CF.Key.set(KEY)
 BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
 CF.BaseUrl.set(BASE_URL)
@@ -41,47 +35,51 @@ CF.BaseUrl.set(BASE_URL)
 # default_app = firebase_admin.initialize_app(cred)
 # db = firestore.client
 
+
+# Global variables
 global wait
-wait = 4000
+wait = 4    # set rate (in seconds) for acquiring frames from camera
 global QUIT
-QUIT = 1
+QUIT = True # boolean for terminating camera (maybe app)
 
+
+# Class which runs the functional GUI application.
 class ScreenOne(Screen):
-
+    # This function runs when the class object is set to run.
     def __init__(self, **kwargs):
         super(ScreenOne, self).__init__(**kwargs)
 
-    #this is event that is fired when the screen is displayed.
+    # This is event that is fired when the screen is displayed.
     def on_enter(self, *args):
         # Initialize a lock object
         global lock
         lock = threading.Lock()
-
         # Start a separate thread for user recognition
+        global camera_thread
+        camera_thread = threading.Thread(target=self.acquireImage)
+        camera_thread.start()
+        camera_thread.join()
+        # Start a separate thread for GUI
+        global main_thread
+        main_thread = threading.Thread(target=self.set_init_widgets)
+        main_thread.start()
+        main_thread.join()
 
-        # Change the various labels
-        # self.
-        # self.acquireImage()
-        pass
-
-    def displayScreenThenLeave(self):
-        pass
-        # schedued after 3 seconds
-
-        # Clock.schedule_once(self.changeScreen, 4)
-        # Clock.schedule_once(self.acquireImage, 4)
-
+    # Function for acquiring images/frames from environment
     def acquireImage(self):
-        check_photo = 1
-
-        while(QUIT == 1):
+        while(QUIT == True):
+            # Code for waiting after frame (integer value to mili-seconds)
+            time.sleep(wait)
             # Capture frame-by-frame
             ret, frame = cap.read()
             if ret == True:
+                print('camera')
                 # Our operations on the frame come here
                 color_obj = cv2.cvtColor(frame, cv2.COLORMAP_BONE)
                 # Write the frame into the file newImage.jpg
-                cv2.imwrite('newImage.png', color_obj)
+                cv2.imwrite('environment_image.png', color_obj)
+                check = main_thread.isAlive()
+                print(check)
                 # faces = CF.face.detect('newImage.jpg')
                 # Communicate with user faces database here
                 #
@@ -91,12 +89,13 @@ class ScreenOne(Screen):
 
                 # self.center_image.source = 'newImage.png'
 
-            # Code for waiting after frame (integer value to mili-seconds)
-            cv2.waitKey(wait)
 
         # self.ids.label_1_sc1.color = 0,1,0,1
         # self.ids.label_1_sc1.text = "User Found..."
 
+    # Use this function for changing the text parameters when a user has been found
+    # via function acquireImage.
+    @mainthread
     def changeTexts(self):
         self.ids.categ_button.text = "Category:______"
         self.ids.categ_button.color = 0.54,0.824,1, 1
@@ -106,17 +105,51 @@ class ScreenOne(Screen):
         self.ids.right_button.text = ">"
         self.ids.timer_label.text = "0 sec"
 
+    # Use this function for changing the
+    @mainthread
     def set_init_widgets(self):
-        # self.ids.
+        # Bind buttons on GUI to specific functions
+        self.ids.left_button.bind(on_press=self.go_left)
+        self.ids.right_button.bind(on_press=self.go_right)
+        self.ids.quit_button.bind(on_press=self.quit_main)
+
+        # Instantiate DropDown button
+        dropdown = CustomDropDown()
+        self.ids.categ_button.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=lambda instance, x: setattr(self.ids.categ_button, 'text', x))
+
+        # Disable category, go left, go right, and quit buttons at start
+        self.ids.left_button.disabled = True
+        self.ids.right_button.disabled = True
+        #self.ids.categ_button.disabled = True
+        self.ids.quit_button.disabled = True
+
+    @mainthread
+    def go_left(self):
         pass
 
-class Manager(ScreenManager):
+    @mainthread
+    def go_right(self):
+        pass
 
+    @mainthread
+    def quit_main(self):
+        print('penis')
+        pass
+
+class CustomDropDown(DropDown):
+    pass
+
+class Manager(ScreenManager):
+    stop = threading.Event()
     screen_one = ObjectProperty(None)
-    screen_two = ObjectProperty(None)
-    screen_three = ObjectProperty(None)
 
 class ScreensApp(App):
+    def on_stop(self):
+        # The Kivy event loop is about to stop, set a stop signal;
+        #  otherwise the app window will close, but the Python process will
+        # keep running until all secondary threads exit.
+        self.root.stop.set()
 
     def build(self):
         m = Manager(transition=NoTransition())
