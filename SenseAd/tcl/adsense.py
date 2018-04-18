55636bb4908b8e78f374cadb49acfbb443af28dd
danielmachlab <daniel@hadlim.com>
2018-04-18 15:53:18 -0500

    connecting to firestore

--- SenseAd/tcl/adsense.py
+++ SenseAd/tcl/adsense.py
@@ -1,6 +1,37 @@
 from tcl.adsense_classes import Advertisement, Sensors, User
 import operator
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('tcl/keys/distributedadvertisingboard-firebase-adminsdk-uolqk-4575dfc16f.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
+
+doc_ref = db.collection(u'users').document(u'aturing')
+doc_ref.set({
+    u'first': u'Alan',
+    u'middle': u'Mathison',
+    u'last': u'Turing',
+    u'born': 1912
+})
+
+users_ref = db.collection(u'users')
+docs = users_ref.get()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
 ad_data = []
 with open("Advertisements.txt") as file_ads:
     line = file_ads.readline()
