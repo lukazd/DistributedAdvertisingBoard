8d6d7a1ce98bacd69da42ac0439ef705f11d3fc8
Luka Zdilar <zdilarlu@gmail.com>
2018-04-24 15:37:48 -0500

    Add 400 for bad requests

--- SenseAd/app.py
+++ SenseAd/app.py
@@ -8,7 +8,12 @@ def hello():
 @application.route("/getAdsForUser", methods=['GET'])
 def getAdsForUser():
     user_id = request.args.get('user_id')
+
+    if user_id is None:
+        return abort(400)
+
     return "Welcome: " + user_id
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
