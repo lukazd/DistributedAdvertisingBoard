2835ab52d46e296ed7959edf3ac35a19c3f820fb
Luka Zdilar <zdilarlu@gmail.com>
2018-04-24 15:18:56 -0500

    Add api method for getting user ads

--- SenseAd/app.py
+++ SenseAd/app.py
@@ -5,5 +5,10 @@ application = Flask(__name__)
 def hello():
     return "Hello App!"
 
+@application.route("/getAdsForUser", methods=['GET'])
+def getAdsForUser():
+    user_id = request.args.get('user_id')
+    return "Welcome: " + user_id
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
