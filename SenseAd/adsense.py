42d05f0be3aa17eaed9c4f2f36e1dee8079cfd0a
Daniel Machlab <daniel@hadlim.com>
2018-04-25 14:03:53 -0500

    finished random forest with iris data

--- SenseAd/adsense.py
+++ SenseAd/adsense.py
@@ -21,6 +21,61 @@ db = firestore.Client()
 users_ref = db.collection(u'carAds')
 docs = users_ref.get()
 
+df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+
+print(df.head())
+
+#creates two new dataframes, one with the training rows, one witht the test rows
+train, test = df[df['is_train']==True], df[df['is_train']==False]
+print('Number of observations in the training data:', len(train))
+print('Number of observation in the test data:', len(test))
+features = df.columns[:4]
+print(features)
+
+#convert the species name into digit - 0,1, or 2
+#for sensead, we will have categories instead of species
+y = pd.factorize(train['species'])[0]
+print('y: ', y)
+
+#create RandomForestClassifier, clf = Classifier
+clf = RandomForestClassifier(n_jobs=2, random_state=0)
+
+#train the classifier to take the training features and learn how they
+#relater to the trainging y (the species)
+clf.fit(train[features], y)
+print('clf: ',clf)
+
+#applies classifier train above to the test data, this test accuracy of model
+#array shoes what species the model predicts each plant is based on
+#the four data points, septal length/width and petal legth/width
+print('test preditctions: ', clf.predict(test[features]))
+
+#Shows the predicted probability of the first 10 observations
+print('prediction probability', clf.predict_proba(test[features])[0:10])
+
+#EVALUATION OF CLASSIFIER
+#create english names for the plants for each predicted plant class
+preds = iris.target_names[clf.predict(test[features])]
+
+#print PREDICTED species for the first five observations
+print(preds[0:5])
+
+#View the ACTUAL species for the first 5 observations
+print(test['species'].head())
+
+#create a confusion matrix to test the rest of the data
+#Columns are the species we predicted for the test data
+#Rows are the actual species for the test data
+print(pd.crosstab(test['species'], preds, rownames=['Actual Species'], colnames=['Predicted Species']))
+
+#View a list of the features and their importance scores
+print(list(zip(train[features], clf.feature_importances_)))
+
+
+
+
+
+
 #for doc in docs:
     #print(u'{} => {}'.format(doc.id, doc.to_dict()))
 
