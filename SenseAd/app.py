32aaa678019d58d2d17198434c09434defbd05a0
Luka Zdilar <zdilarlu@gmail.com>
2018-04-29 13:15:53 -0500

    Fix sampling

--- SenseAd/app.py
+++ SenseAd/app.py
@@ -34,7 +34,7 @@ def getAdsForUser():
     print("recommendations")
     print(recommendations)
     if recommendations is None:
-        random_docs = random.sample(docs, 10)
+        random_docs = random.sample(list(docs), 10)
         for doc in random_docs:
             recommendations.append(doc.id)
 
